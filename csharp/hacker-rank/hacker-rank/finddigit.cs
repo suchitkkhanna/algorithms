d6a00d695d7cfa567db9b0ba3d336e54b03b5653
suchit <khanna.suchit@gmail.com>
2014-12-26 23:37:06 +0530

    -adding in the folders for csharp -checking in the hacker-rank solution

---
+++ csharp/hacker-rank/hacker-rank/finddigit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace hacker_rank
+{
+    internal class FindDigit
+    {
+        static void Main(string[] args)
+        {
+            string numberOfInputs = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(numberOfInputs))
+                return;
+
+            numberOfInputs = numberOfInputs.Trim();
+
+            int number;
+
+            if (!int.TryParse(numberOfInputs, out number) || number == 0)
+                return;
+
+            var results = new List<int>();
+
+            while (number > 0)
+            {
+                int count = 0;
+
+                string line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line)) return;
+
+                int inputNumber;
+
+                if (!int.TryParse(line, out inputNumber)) return;
+
+                int temp = inputNumber;
+                while (temp > 0)
+                {
+                    int digit = temp % 10;
+
+                    if (digit != 0 && inputNumber % digit == 0)
+                    {
+                        count++;
+                    }
+                    temp = temp / 10;
+                }
+
+                number--;
+
+                results.Add(count);
+            }
+
+            foreach (var result in results)
+                Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
